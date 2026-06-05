3da8d6b3f6e96be4ec6e236055aff208175ebe11
Caleb <ctenhuisen@gmail.com>
2026-06-05 22:09:48 +0000

    Fix crash on missing GOOGLE_PRIVATE_KEY env var

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const SHEET_NAME = 'RiskAssessments';
 const auth = new google.auth.JWT(
     process.env.GOOGLE_CLIENT_EMAIL,
     null,
-    process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
     ['https://www.googleapis.com/auth/spreadsheets']
 );
 
